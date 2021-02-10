73cfe6e29e7a1696721f5f93e5a89c5512a307ca
Marco Boneberger <m.boneberger@tu-bs.de>
2021-02-10 17:37:12 +0100

    add READMe.md add license and update Cargo.toml's

--- rubullet/src/lib.rs
+++ rubullet/src/lib.rs
@@ -1,4 +1,41 @@
 //! A Rust interface for Bullet physics inspired by PyBullet.
+//!
+//! # Example
+//! ```rust
+//! use std::{thread, time::Duration};
+//!
+//! use easy_error::Terminator;
+//! use nalgebra::{Isometry3, Vector3};
+//! use rubullet::*;
+//!
+//! fn main() -> Result<(), Terminator> {
+//!     let mut physics_client = PhysicsClient::connect(Mode::Gui)?;
+//!
+//!     physics_client.set_additional_search_path("../rubullet-ffi/bullet3/libbullet3/data")?;
+//!     physics_client.set_gravity(Vector3::new(0.0, 0.0, -10.0))?;
+//!
+//!     let _plane_id = physics_client.load_urdf("plane.urdf", Default::default())?;
+//!
+//!     let cube_start_position = Isometry3::translation(0.0, 0.0, 1.0);
+//!     let box_id = physics_client.load_urdf(
+//!         "r2d2.urdf",
+//!         UrdfOptions {
+//!             base_transform: cube_start_position,
+//!             ..Default::default()
+//!         },
+//!     )?;
+//!
+//!     for _ in 0..10000 {
+//!         physics_client.step_simulation()?;
+//!         thread::sleep(Duration::from_micros(4167));
+//!     }
+//!
+//!     let cube_transform = physics_client.get_base_transform(box_id)?;
+//!     println!("{}", cube_transform);
+//!
+//!     Ok(())
+//! }
+//! ```
 #![allow(dead_code)]
 
 pub use crate::{
